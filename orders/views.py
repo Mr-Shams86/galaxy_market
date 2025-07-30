d25dc9cb181723ad4ae5a7f70191298e67e2d7b2
Samer <samershams26@gmail.com>
2025-07-30 21:49:42 +0500

    Реализованна логика добовления товара в корзину

--- orders/views.py
+++ orders/views.py
@@ -1,13 +1,24 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from products.models.product import Product
 
 
 def cart_view(request):
-    return render(request, "orders/cart.html")
+    cart = request.session.get('cart', [])
+    products = Product.objects.filter(id__in=cart)
+    return render(request, "orders/cart.html", {"products": products})
 
 
 def checkout_view(request):
     return render(request, "orders/checkout.html")
+
+
+def add_to_cart(request, product_id):
+    cart = request.session.get('cart', [])
+    if product_id not in cart:
+        cart.append(product_id)
+        request.session['cart'] = cart
+    return redirect('orders:cart')
 
 
 @login_required
